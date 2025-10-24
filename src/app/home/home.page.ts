c4be5277d2eafd8b6a8df46f6bf0da1552c597e5
Jose Luis Riquelme <jose.riquelmes@usach.cl>
2025-10-24 08:42:57 -0300

    making changes in the contacts-plugin passing it from java to kotlin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList,IonItem, IonLabel } from '@ionic/angular/standalone';
+import {registerPlugin} from '@capacitor/core';
+const ContactsPlugin = registerPlugin<any>('ContactsPlugin');
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonLabel, IonList, IonButton, IonHeader, IonToolbar, IonTitle, IonContent,IonItem]
 })
 export class HomePage {
+  contacts:{name: string; number:string}[]= [];
   constructor() {}
+  async loadContacts(){
+    try{
+    const result = await ContactsPlugin.getContacts();
+    console.log("Resultado del plugin", result);
+      this.contacts = result?.contacts?.map((c: any)=>({
+        name: c.name??'',
+        number: c.number??''
+      })) ??[];
+      console.log('Contacts:',this.contacts)
+    }catch(err){
+      console.error('Error obteniendo contactos desde js:', err);
+      this.contacts = [];
+    }
+  }
 }
